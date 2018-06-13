c64a5423ff104a1d64a7263c8b93d395d5af60b5
Wannes <wannes@ramsit.com>
2018-06-13 15:17:25 +0200

    Added Community create message

--- communities/hideCreateCommunityButton.js
+++ communities/hideCreateCommunityButton.js
@@ -8,6 +8,6 @@ if(typeof(dojo) != "undefined") {
 		//get ID with query("#id) and add style
 	   dojo.query("#createPlaceButton").style("display","none");
         });
-				dojo.query("div.lotusActionBar.lotusBtnContainer")[0].textContent="Creating Communities is not allowed, please contact your admin!";
+				dojo.query("div.lotusActionBar.lotusBtnContainer")[0].textContent="Creating Communities is not allowed, please contact your Admin!";
 			}
 }
