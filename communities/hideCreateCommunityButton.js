eecb4997f77c2675242b9ebd8cd613fccf27b9db
Wannes <wannes@ramsit.com>
2018-07-13 21:23:34 +0200

    Added JS template

--- communities/hideCreateCommunityButton.js
+++ communities/hideCreateCommunityButton.js
@@ -1,11 +1,42 @@
 // ==UserScript==
-// @name         Hide Create Community Button
-// @description  Hide the Create a community Button!
+//
+// @name         hideCreateCommunityButton
+// @version      1.0
+// @description  Hide the Create Community button except for a selected group of users
+//
+// @author       Wannes Rams
+//
 // ==/UserScript==
 
 if(typeof(dojo) != "undefined") {
-	require(["dojo/domReady!"],  function(){
-		//get ID with query("#id) and add style
-	   dojo.query("#createPlaceButton").style("display","none");
-        })
+	require(["dojo/domReady!"], function(){
+        try {
+            // utility function to let us wait for a specific element of the page to load...
+            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
+                if(!elXpathRoot) var elXpathRoot = dojo.body();
+                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
+                if(!waitTime) var waitTime = 1;  // 1000=1 second
+                if(!elXpath) return;
+                var waitInter = 0;  // current interval
+                var intId = setInterval( function(){
+                    if( ++waitInter<maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
+
+                    clearInterval(intId);
+                    if( waitInter >= maxInter) {
+                        console.log("**** WAITFOR ["+elXpath+"] WATCH EXPIRED!!! interval "+waitInter+" (max:"+maxInter+")");
+                    } else {
+                        console.log("**** WAITFOR ["+elXpath+"] WATCH TRIPPED AT interval "+waitInter+" (max:"+maxInter+")");
+                        callback();
+                    }
+                }, waitTime);
+            };
+
+            // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
+            // before we proceed to customize the page...
+            waitFor( function(){
+			// wait until the "loading..." node has been hidden
+			// indicating that we have loaded content.
+   			dojo.query("#createPlaceButton").style("display","none");
+       	          }
+   });
 }
