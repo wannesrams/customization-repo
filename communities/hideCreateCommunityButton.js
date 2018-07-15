f560ca19ed08d204ee04876b62d347ecc699372b
wannesrams <palm35@gmail.com>
2018-07-15 19:12:03 +0200

    Update hideCreateCommunityButton.js

--- communities/hideCreateCommunityButton.js
+++ communities/hideCreateCommunityButton.js
@@ -12,8 +12,11 @@ if(typeof(dojo) != "undefined") {
 	require(["dojo/domReady!"], function(){
 		var currentUserEmail = lconnCurrentUser.email;
 		console.log(currentUserEmail);
-		if(lconnCurrentUser.email != "wannes@ramsit.com" || lconnCurrentUser.email != "maria@shinythingzz.com"){
-   		dojo.query("#createPlaceButton").style("display","none");
-			}
+		if(currentUserEmail == "wannes@ramsit.com" || currentUserEmail == "maria@shinythingzz.com" ){
+			console.log("Showing Create Community button");
+		} else{
+			dojo.query("#createPlaceButton").style("display","none");
+			console.log("Showing Create Community button");
+		}
 		});
 }
